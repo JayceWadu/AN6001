b078665c18916e14b412d62b5a0cb3adc0c49b15
JayceWadu <111266741+JayceWadu@users.noreply.github.com>
2022-08-15 14:40:37 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,8 @@
 #!/usr/bin/env python
 # coding: utf-8
-
-# In[1]:
-
-
 from flask import Flask, render_template, request
-
-
-# In[2]:
-
-
-app = Flask(__name__)
-
-
-# In[3]:
-
-
 import joblib
-
+app = Flask(__name__)
 @app.route("/", methods = ["GET", "POST"])
 def index():
     if request.method == 'POST':
@@ -30,29 +15,5 @@ def index():
         return(render_template('index.html', result1 = r1, result2 = r2))
     else:
         return(render_template('index.html', result1 = 'waiting', result2 = 'waiting'))
-
-
-# In[ ]:
-
-
 if __name__ == '__main__':
     app.run()
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
