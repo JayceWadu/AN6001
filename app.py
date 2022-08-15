b02bcfeea3410105686f7c430132a97d1af6f92d
JayceWadu <111266741+JayceWadu@users.noreply.github.com>
2022-08-15 14:47:35 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,18 @@
-#!/usr/bin/env python
-# coding: utf-8
 from flask import Flask, render_template, request
-import joblib
 app = Flask(__name__)
-@app.route("/", methods = ["GET", "POST"])
+import joblib
+
+@app.route("/", methods=["GET","POST"])
 def index():
-    if request.method == 'POST':
+    if request.method == "POST":
         rates = float(request.form.get("rates"))
         print(rates)
-        model1 = joblib.load('regression_DBS')
+        model1 = joblib.load("regression_DBS")
         r1 = model1.predict([[rates]])
-        model2 = joblib.load('Tree')
+        model2 = joblib.load("tree_DBS")
         r2 = model2.predict([[rates]])
-        return(render_template('index.html', result1 = r1, result2 = r2))
+        return(render_template("index.html",result1=r1,result2=r2))
     else:
-        return(render_template('index.html', result1 = 'waiting', result2 = 'waiting'))
-if __name__ == '__main__':
+        return(render_template("index.html",result1="waiting",result2="waiting"))
+if __name__ == "__main__":
     app.run()
